594d0a46d82b64e4565eb8dc4f9ae5696ed3f895
Samuel Padgett <spadgett@us.ibm.com>
2011-11-10 10:46:09 -0500

    Bug 363485 - [Bugzilla] Fix query response resource type and URI

--- org.eclipse.lyo.samples.bugzilla/src/main/java/org/eclipse/lyo/samples/bugzilla/ChangeRequestCollectionService.java
+++ org.eclipse.lyo.samples.bugzilla/src/main/java/org/eclipse/lyo/samples/bugzilla/ChangeRequestCollectionService.java
@@ -39,7 +39,7 @@ import org.eclipse.lyo.samples.bugzilla.jbugzx.rpc.ExtendedBugSearch;
 import org.eclipse.lyo.samples.bugzilla.jbugzx.rpc.GetProducts;
 import org.eclipse.lyo.samples.bugzilla.resources.BugzillaChangeRequest;
 import org.eclipse.lyo.samples.bugzilla.resources.Person;
-import org.eclipse.lyo.samples.bugzilla.resources.QueryResult;
+import org.eclipse.lyo.samples.bugzilla.resources.QueryResponse;
 import org.eclipse.lyo.samples.bugzilla.resources.ResponseInfo;
 import org.eclipse.lyo.samples.bugzilla.utils.AcceptType;
 import org.eclipse.lyo.samples.bugzilla.utils.RdfUtils;
@@ -194,8 +194,11 @@ public class ChangeRequestCollectionService extends HttpServlet {
 				responseInfo.setUri(new URI(request.getRequestURL().append('?')
 						.append(request.getQueryString()).toString()));
 
-				QueryResult queryResult = new QueryResult();
-				queryResult.setUri(new URI(request.getRequestURL().toString()));
+				QueryResponse queryResult = new QueryResponse();
+				// This must match the query capability base.
+				queryResult.setUri(new URI(request.getRequestURL()
+						.append("?productId=").append(product.getId())
+						.toString()));
 				for (Bug bug : results) {
 					BugzillaChangeRequest changeRequest = BugzillaChangeRequest.fromBug(bug);
 					queryResult.getMembers().add(changeRequest);
