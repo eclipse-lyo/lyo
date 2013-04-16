af33d7270b77a315ae855b6c80c00380c3e1219e
Michael Fiedler <fiedler.mf@gmail.com>
2013-04-16 10:43:17 -0400

    ASSIGNED - bug 405503: Add client support for Architecture Management and Rational Design Manager https://bugs.eclipse.org/bugs/show_bug.cgi?id=405503
    Change-Id: Icd535c1a09d9f7fd47f5c8876d83d369cd040c8c
    Signed-off-by: Michael Fiedler <fiedler.mf@gmail.com>

---
+++ org.eclipse.lyo.client.java/src/main/java/org/eclipse/lyo/client/oslc/resources/ArchitectureConstants.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ *  
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *
+ *     Michael Fiedler        - initial implementation
+ *******************************************************************************/
+package org.eclipse.lyo.client.oslc.resources;
+
+
+public interface ArchitectureConstants
+{
+	public static String ARCHITECTURE_DOMAIN        = "http://open-services.net/ns/am#";
+	public static String ARCHITECTURE_NAMESPACE     = "http://open-services.net/ns/am#";
+	public static String ARCHITECTURE_PREFIX        = "oslc_am";
+    public static String FOAF_NAMESPACE                              = "http://xmlns.com/foaf/0.1/";
+    public static String FOAF_NAMESPACE_PREFIX                       = "foaf";
+
+    public static String TYPE_ARCHITECTURE_RESOURCE  = ARCHITECTURE_NAMESPACE + "Resource";
+    public static String TYPE_ARCHITECTURE_LINK_TYPE = ARCHITECTURE_NAMESPACE + "LinkType";
+    public static String TYPE_PERSON                = FOAF_NAMESPACE + "Person";
+
+}
