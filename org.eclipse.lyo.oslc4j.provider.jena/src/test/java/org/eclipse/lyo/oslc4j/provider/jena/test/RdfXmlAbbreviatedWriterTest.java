4c340170a638faaf2d45915fb5a99e00a57ad8c8
Samuel Padgett <spadgett@us.ibm.com>
2013-07-29 15:41:00 -0400

    Bug 407902: Add JUnit test for abbreviated writer circular references
    Update the Jena provider project to use Maven's standard directory
    structure so we can add a JUnit. Add a test for the circular reference.
    
    Change-Id: Iaeffc9a9f19ebec83f9e3efd0253145cc253cbf3
    Signed-off-by: Samuel Padgett <spadgett@us.ibm.com>

---
+++ org.eclipse.lyo.oslc4j.provider.jena/src/test/java/org/eclipse/lyo/oslc4j/provider/jena/test/RdfXmlAbbreviatedWriterTest.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ *
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *
+ *     Samuel Padgett - initial implementation
+ *******************************************************************************/
+package org.eclipse.lyo.oslc4j.provider.jena.test;
+
+import java.io.InputStream;
+
+import org.eclipse.lyo.oslc4j.provider.jena.RdfXmlAbbreviatedWriter;
+import org.junit.Test;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+public class RdfXmlAbbreviatedWriterTest {
+	@Test
+	public void testCircularReference()
+	{
+		InputStream is = RdfXmlAbbreviatedWriterTest.class.getResourceAsStream("circular.xml");
+		Model m = ModelFactory.createDefaultModel();
+		m.read(is, null);
+		RdfXmlAbbreviatedWriter w = new RdfXmlAbbreviatedWriter();
+		w.write(m, System.out, null);
+	}
+}
