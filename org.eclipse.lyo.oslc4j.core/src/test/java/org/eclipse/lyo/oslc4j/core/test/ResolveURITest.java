0eaa4e1ee06a2c9bfd0c3b114577dbc78d72fd77
Samuel Padgett <spadgett@us.ibm.com>
2013-10-29 09:57:30 -0400

    Bug 420620 - Problems with ResolveURITest in oslc4j.core
    Make sure we're not using a cached public URI root in
    testDisableHostResuloution() and fix other problems with the test.
    
    Change-Id: I667a6c77fdc2cd3fd57211b6e28fedcc90fdabef
    Signed-off-by: Samuel Padgett <spadgett@us.ibm.com>

--- org.eclipse.lyo.oslc4j.core/src/test/java/org/eclipse/lyo/oslc4j/core/test/ResolveURITest.java
+++ org.eclipse.lyo.oslc4j.core/src/test/java/org/eclipse/lyo/oslc4j/core/test/ResolveURITest.java
@@ -12,11 +12,14 @@
  * Contributors:
  *
  *     Samuel Padgett - initial implementation
+ *     Samuel Padgett - fix test problems from cached public URI root
  *******************************************************************************/
 package org.eclipse.lyo.oslc4j.core.test;
 
 import static org.mockito.Mockito.when;
 import static org.junit.Assert.*;
+
+import java.net.MalformedURLException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -34,17 +37,17 @@ import org.mockito.Mockito;
  */
 public class ResolveURITest {
 	@Before
-	public void clearPublicURISystemProperty() {
-		System.clearProperty(OSLC4JConstants.OSLC4J_PUBLIC_URI);
+	public void clearPublicURISystemProperty() throws MalformedURLException {
 		System.clearProperty(OSLC4JConstants.OSLC4J_DISABLE_HOST_RESOLUTION);
+		OSLC4JUtils.setPublicURI(null);
 	}
 
 	@Test
-	public void testPublicURI() {
+	public void testPublicURI() throws MalformedURLException {
 		 HttpServletRequest  mockedRequest = Mockito.mock(HttpServletRequest.class);
 		 when(mockedRequest.getServletPath()).thenReturn("/resources");
 		 when(mockedRequest.getPathInfo()).thenReturn("/bugs/1");
-		 System.setProperty(OSLC4JConstants.OSLC4J_PUBLIC_URI, "http://hostname.example.com:12357/myapp/");
+		 OSLC4JUtils.setPublicURI("http://hostname.example.com:12357/myapp/");
 		 String resolvedUri = OSLC4JUtils.resolveURI(mockedRequest, true);
 		 assertEquals("http://hostname.example.com:12357/myapp/resources/bugs/1", resolvedUri);
 	}
@@ -52,7 +55,7 @@ public class ResolveURITest {
 	@Test
 	public void testDisableHostResolution() {
 		 HttpServletRequest  mockedRequest = Mockito.mock(HttpServletRequest.class);
-		 when(mockedRequest.getScheme()).thenReturn("https://");
+		 when(mockedRequest.getScheme()).thenReturn("https");
 		 when(mockedRequest.getServerName()).thenReturn("hostname.example.com");
 		 when(mockedRequest.getServerPort()).thenReturn(12357);
 		 when(mockedRequest.getContextPath()).thenReturn("/myapp");
@@ -60,6 +63,6 @@ public class ResolveURITest {
 		 when(mockedRequest.getPathInfo()).thenReturn("/bugs/1");
 		 System.setProperty(OSLC4JConstants.OSLC4J_DISABLE_HOST_RESOLUTION, "true");
 		 String resolvedUri = OSLC4JUtils.resolveURI(mockedRequest, true);
-		 assertEquals("http://hostname.example.com:12357/myapp/resources/bugs/1", resolvedUri);
+		 assertEquals("https://hostname.example.com:12357/myapp/resources/bugs/1", resolvedUri);
 	}
 }
