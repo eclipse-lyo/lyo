345018c8b644b56a3341d37a2286c689c269c25b
Samuel Padgett <spadgett@us.ibm.com>
2014-08-06 16:56:28 -0400

    Bug 441291 - Unit tests should not actually connect to example.com
    Change-Id: I77963c662c676995c94bb1075be6c06343677d45
    Signed-off-by: Samuel Padgett <spadgett@us.ibm.com>

--- org.eclipse.lyo.client.java/src/test/java/org/eclipse/lyo/client/test/OslcClientTest.java
+++ org.eclipse.lyo.client.java/src/test/java/org/eclipse/lyo/client/test/OslcClientTest.java
@@ -27,6 +27,7 @@ import org.apache.wink.client.ClientRuntimeException;
 import org.eclipse.lyo.client.oslc.OSLCConstants;
 import org.eclipse.lyo.client.oslc.OslcClient;
 import org.eclipse.lyo.client.oslc.resources.AutomationRequest;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class OslcClientTest {
@@ -34,6 +35,7 @@ public class OslcClientTest {
 	 * Tests that the RDF/XML MessageBodyWriter doesn't go into an infinite loop when
 	 * given bad data on the client (Bug 417749). ClientRuntimeException expected.
 	 */
+	@Ignore // ignore this test because it actually connects to example.com, which fails in our Hudson environment
 	@Test(expected = ClientRuntimeException.class, timeout = 5000)
 	public void postInvalidOlscResource() throws IOException, OAuthException, URISyntaxException {
 		final OslcClient client = new OslcClient();
