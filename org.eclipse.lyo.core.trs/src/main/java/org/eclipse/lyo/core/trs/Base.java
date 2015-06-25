bc59588dce8111a739c2f65edcd3c32332ea5e38
Jim Ruehlin <jruehlin@us.ibm.com>
2015-06-25 08:40:16 -0700

    Bug 463217: Fixes for Javadoc warnings in lyo.core.trs.
    Change-Id: I77e7af563f1338c4560b6f8090520b5aaf8a711a
    Signed-off-by: Jim Ruehlin <jruehlin@us.ibm.com>

--- org.eclipse.lyo.core.trs/src/main/java/org/eclipse/lyo/core/trs/Base.java
+++ org.eclipse.lyo.core.trs/src/main/java/org/eclipse/lyo/core/trs/Base.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBM Corporation.
+ * Copyright (c) 2013, 2015 IBM Corporation.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -169,7 +169,7 @@ public class Base extends AbstractResource {
 		return cutoffEvent;
 	}
 	/**
-	 * @param cutoffIdentifier the cutoffIdentifier to set
+	 * @param cutoffEvent the cutoffEvent to set
 	 */
 	public void setCutoffEvent(URI cutoffEvent) {
 		this.cutoffEvent = cutoffEvent;
--- org.eclipse.lyo.core.trs/src/main/java/org/eclipse/lyo/core/trs/ChangeLog.java
+++ org.eclipse.lyo.core.trs/src/main/java/org/eclipse/lyo/core/trs/ChangeLog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBM Corporation.
+ * Copyright (c) 2013, 2015 IBM Corporation.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -145,7 +145,7 @@ public class ChangeLog extends AbstractChangeLog
 	}
 	
 	/**
-	 * @param changes the changes to set
+	 * @param change the changes to set
 	 */
 	public void setChange(List<ChangeEvent> change) {
 		this.change = change;
