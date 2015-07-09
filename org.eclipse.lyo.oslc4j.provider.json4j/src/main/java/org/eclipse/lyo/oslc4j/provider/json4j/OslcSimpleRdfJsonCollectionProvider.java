ecdf58b28e66b24ca156fb5a4f978e8952784491
Jim Ruehlin <jruehlin@us.ibm.com>
2015-07-09 12:53:45 -0700

    Bug 472228 - Compiler warnings in org.eclipse.lyo.oslc4j.proider.json4j
    Change-Id: I2420727a6f9897fc13ef6662ad5b23b0d4722491
    Signed-off-by: Jim Ruehlin <jruehlin@us.ibm.com>

--- org.eclipse.lyo.oslc4j.provider.json4j/src/main/java/org/eclipse/lyo/oslc4j/provider/json4j/OslcSimpleRdfJsonCollectionProvider.java
+++ org.eclipse.lyo.oslc4j.provider.json4j/src/main/java/org/eclipse/lyo/oslc4j/provider/json4j/OslcSimpleRdfJsonCollectionProvider.java
@@ -19,34 +19,16 @@
 package org.eclipse.lyo.oslc4j.provider.json4j;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.AbstractCollection;
-import java.util.AbstractList;
-import java.util.AbstractSequentialList;
-import java.util.AbstractSet;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.NavigableSet;
-import java.util.Queue;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.ext.MessageBodyReader;
-import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
 import org.eclipse.lyo.oslc4j.core.model.OslcMediaType;
