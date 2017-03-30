8b2fe91514ea8c1919eb5faf3f7ffc44258e5208
Andrew Berezovskyi <andriib@kth.se>
2017-03-30 11:03:15 +0200

    Bug 514456 - Add equality methods to Link
    equals() and hashCode() compare the URI & the label. The unit test
    verifies individual behaviour as well as the operations on the
    HashSet<Link>.
    
    Change-Id: I589dabc8c4c995777d78ef802096b428c09b4ad0
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- org.eclipse.lyo.oslc4j.core/src/main/java/org/eclipse/lyo/oslc4j/core/model/Link.java
+++ org.eclipse.lyo.oslc4j.core/src/main/java/org/eclipse/lyo/oslc4j/core/model/Link.java
@@ -4,7 +4,7 @@
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
- *	
+ *
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
@@ -12,58 +12,68 @@
  * Contributors:
  *
  *	   Samuel Padgett - initial API and implementation
+ *	   Andrew Berezovskyi - basic test code
  *******************************************************************************/
 package org.eclipse.lyo.oslc4j.core.model;
 
 import java.net.URI;
-
+import java.util.Objects;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcName;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcPropertyDefinition;
 
 /**
  * Special OSLC link type. Differs from {@link URI} since it can hold a label,
  * expressed in RDF using reification.
- * 
- * @see <a href="http://open-services.net/bin/view/Main/OslcCoreSpecAppendixLinks">OSLC Core Specification 2.0, Appendix C: Guidance on Links & Relationships</a>
+ * @see <a href="http://open-services.net/bin/view/Main/OslcCoreSpecAppendixLinks">OSLC Core
+ * Specification 2.0, Appendix C: Guidance on Links & Relationships</a>
  */
-public class Link extends AbstractReifiedResource<URI>
-{
-	private String label;
+public class Link extends AbstractReifiedResource<URI> {
+    private String label;
 
-	public Link()
-	{	
-	}
-	
-	public Link(URI resource)
-	{
-		setValue(resource);
-	}
-	
-	public Link(URI resource, String label)
-	{
-		setValue(resource);
-		this.label = label;
-	}
+    public Link() {
+    }
 
-	@OslcName("title")
-	@OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "title")
-	/**
-	 * Gets the link label.
-	 * 
-	 * @return the label
-	 */
-	public String getLabel()
-	{
-		return label;
-	}
+    public Link(URI resource) {
+        setValue(resource);
+    }
 
-	/**
-	 * Sets the link label.
-	 * 
-	 * @param label the label
-	 */
-	public void setLabel(String label)
-	{
-		this.label = label;
-	}	
+    public Link(URI resource, String label) {
+        setValue(resource);
+        this.label = label;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Link)) {
+            return false;
+        }
+        final Link link = (Link) o;
+        return Objects.equals(this.getValue(), link.getValue());
+    }
+
+    @OslcName("title")
+    @OslcPropertyDefinition(OslcConstants.DCTERMS_NAMESPACE + "title")
+    /**
+     * Gets the link label.
+     *
+     * @return the label
+     */ public String getLabel() {
+        return label;
+    }
+
+    /**
+     * Sets the link label.
+     * @param label the label
+     */
+    public void setLabel(String label) {
+        this.label = label;
+    }
 }
