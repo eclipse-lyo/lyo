4bc3f47273e0e14e2ceb2496a6a334778e8df61d
Andrew Berezovskyi <andriib@kth.se>
2017-07-07 14:06:53 +0200

    Bug 441964 - Resolve "Provider annotation was found on the superclass"
    Add @Provider annotation to the implementing provider classes directly.
    
    Change-Id: I09b498ab43f2212d84e01013569428effd7d5232
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- org.eclipse.lyo.oslc4j.provider.jena/src/main/java/org/eclipse/lyo/oslc4j/provider/jena/OslcTurtleProvider.java
+++ org.eclipse.lyo.oslc4j.provider.jena/src/main/java/org/eclipse/lyo/oslc4j/provider/jena/OslcTurtleProvider.java
@@ -19,8 +19,10 @@ package org.eclipse.lyo.oslc4j.provider.jena;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 
+import javax.ws.rs.ext.Provider;
 import org.eclipse.lyo.oslc4j.core.model.OslcMediaType;
 
+@Provider
 @Produces({OslcMediaType.TEXT_TURTLE})
 @Consumes({OslcMediaType.TEXT_TURTLE})
 public class OslcTurtleProvider extends OslcRdfXmlProvider {
--- org.eclipse.lyo.oslc4j.provider.jena/src/main/java/org/eclipse/lyo/oslc4j/provider/jena/OslcXmlArrayProvider.java
+++ org.eclipse.lyo.oslc4j.provider.jena/src/main/java/org/eclipse/lyo/oslc4j/provider/jena/OslcXmlArrayProvider.java
@@ -19,8 +19,10 @@ package org.eclipse.lyo.oslc4j.provider.jena;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 
+import javax.ws.rs.ext.Provider;
 import org.eclipse.lyo.oslc4j.core.model.OslcMediaType;
 
+@Provider
 @Produces({OslcMediaType.APPLICATION_XML, OslcMediaType.TEXT_XML})
 @Consumes({OslcMediaType.APPLICATION_XML, OslcMediaType.TEXT_XML})
 public class OslcXmlArrayProvider extends OslcRdfXmlArrayProvider {
