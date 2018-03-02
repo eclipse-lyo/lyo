edcdbb23f878cb97700b960139a752ed3acc49d8
Andrew Berezovskyi <andriib@kth.se>
2018-03-02 19:22:17 +0100

    Add a toString method for ChangeEvent
    Change-Id: I4054c0bde96200c6f0c36125a04bf1c96b79b9e9
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- org.eclipse.lyo.core.trs/src/main/java/org/eclipse/lyo/core/trs/ChangeEvent.java
+++ org.eclipse.lyo.core.trs/src/main/java/org/eclipse/lyo/core/trs/ChangeEvent.java
@@ -1,35 +1,37 @@
-/*******************************************************************************
- * Copyright (c) 2013 IBM Corporation, 2017 KTH Royal Institute of Technology
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
- *
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- * http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *
- *	  Kevin Bauer           -  Initial implementation
- *	  Andrew Berezovskyi    -  Dropped abstract modifier (487952)
- *******************************************************************************/
+/*
+ Copyright (c) 2013 IBM Corporation, 2017 KTH Royal Institute of Technology
+
+ All rights reserved. This program and the accompanying materials
+ are made available under the terms of the Eclipse Public License v1.0
+ and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+
+ The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ and the Eclipse Distribution License is available at
+ http://www.eclipse.org/org/documents/edl-v10.php.
+
+ Contributors:
+
+ Kevin Bauer           -  Initial implementation
+ Andrew Berezovskyi    -  Dropped abstract modifier (487952)
+ */
 package org.eclipse.lyo.core.trs;
 
 import java.net.URI;
-
 import org.eclipse.lyo.oslc4j.core.annotation.OslcDescription;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcName;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcPropertyDefinition;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcTitle;
 import org.eclipse.lyo.oslc4j.core.model.AbstractResource;
 
-import static org.eclipse.lyo.core.trs.TRSConstants.*;
+import static org.eclipse.lyo.core.trs.TRSConstants.TRS_CHANGED;
+import static org.eclipse.lyo.core.trs.TRSConstants.TRS_ORDER;
+import static org.eclipse.lyo.core.trs.TRSConstants.TRS_TERM_CHANGED;
+import static org.eclipse.lyo.core.trs.TRSConstants.TRS_TERM_ORDER;
 
 /**
  * Each entry referenced by trs:change is a Local Resource representing a
  * Change Event consisting of the properties contained in this class.
- *
+ * <p>
  * Note: This class cannot be instantiated directly.  Instead create an instance
  * of one of the child classes depending on the type of event taking place.	 The
  * child classes are: Creation, Modification, and Deletion.
@@ -49,6 +51,13 @@ public class ChangeEvent extends AbstractResource {
         super(about);
         this.changed = changed;
         this.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return "ChangeEvent{" + "kind=" + this.getClass()
+                                              .getSimpleName() + ", changed=" + changed + ", " +
+                "order=" + order + '}';
     }
 
     /**
