2ebee64d6865eb5ae95344256f6d0cc22cd9eda6
Andrew Berezovskyi <andriib@kth.se>
2018-05-28 11:37:21 +0200

    Bug 518808 - make AllowedValues.getValues accept non-collection values (#5)
    Contributed by Nick Crossley (ECA)
    
    Change-Id: I60e545bfd138153e05779020521f3a696fa57dd3
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- org.eclipse.lyo.oslc4j.core/src/main/java/org/eclipse/lyo/oslc4j/core/model/AllowedValues.java
+++ org.eclipse.lyo.oslc4j.core/src/main/java/org/eclipse/lyo/oslc4j/core/model/AllowedValues.java
@@ -1,21 +1,22 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2015 IBM Corporation.
+ * Copyright (c) 2012, 2015, 2017 IBM Corporation.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
- *	
+ *
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
  *
- *	   Russell Boykin		- initial API and implementation
- *	   Alberto Giammaria	- initial API and implementation
- *	   Chris Peters			- initial API and implementation
- *	   Gianluca Bernardini	- initial API and implementation
- *	   Samuel Padgett		- allow values other than String
+ *       Russell Boykin        -  initial API and implementation
+ *       Alberto Giammaria     -  initial API and implementation
+ *       Chris Peters          -  initial API and implementation
+ *       Gianluca Bernardini   -  initial API and implementation
+ *       Samuel Padgett        -  allow values other than String
+ *       Nick Crossley         -  handle non-collection values (518808)
  *******************************************************************************/
 package org.eclipse.lyo.oslc4j.core.model;
 
@@ -29,49 +30,53 @@ import org.eclipse.lyo.oslc4j.core.annotation.OslcNamespace;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcResourceShape;
 
 @OslcNamespace(OslcConstants.OSLC_CORE_NAMESPACE)
-@OslcResourceShape(title = "OSLC Allowed Values Resource Shape", describes = OslcConstants.TYPE_ALLOWED_VALUES)
+@OslcResourceShape(title = "OSLC Allowed Values Resource Shape",
+                   describes = OslcConstants.TYPE_ALLOWED_VALUES)
 public final class AllowedValues extends AbstractResource {
-	private static final QName PROPERTY_ALLOWED_VALUE = new QName(OslcConstants.OSLC_CORE_NAMESPACE, "allowedValue");
+    private static final QName PROPERTY_ALLOWED_VALUE = new QName(OslcConstants.OSLC_CORE_NAMESPACE,
+            "allowedValue");
 
-	public AllowedValues() {
-		super();
-	}
-	
-	public Collection<?> getValues() {
-		Collection<?> allowedValues = (Collection<?>) getExtendedProperties().get(PROPERTY_ALLOWED_VALUE);
-		if (allowedValues == null) {
-			return Collections.emptyList();
-		}
-		
-		return allowedValues;
-	}
-	
-	public void setValues(final Collection<? extends Object> values) {
-		getExtendedProperties().put(PROPERTY_ALLOWED_VALUE, values);
-	}
+    public AllowedValues() {
+        super();
+    }
 
-	/**
-	 * @deprecated Use {@link #getValues()}, which allows for values other than String
-	 */
-	@Deprecated
-	public String[] getAllowedValues() {
-		// Be compatible with the old behavior and only include String values.
-		ArrayList<String> stringValues = new ArrayList<String>();
-		Collection<?> values = (Collection<?>) getExtendedProperties().get(PROPERTY_ALLOWED_VALUE);
-		for (Object o : values) {
-			if (o instanceof String) {
-				stringValues.add((String) o);
-			}
-		}
+    public Collection<?> getValues() {
+        Object o = getExtendedProperties().get(PROPERTY_ALLOWED_VALUE);
+        if (o == null) {
+            return Collections.emptyList();
+        } else if (o instanceof Collection) {
+            return (Collection<?>) o;
+        } else {
+            return Collections.singleton(o);
+        }
+    }
 
-		return stringValues.toArray(new String[stringValues.size()]);
-	}
+    public void setValues(final Collection<? extends Object> values) {
+        getExtendedProperties().put(PROPERTY_ALLOWED_VALUE, values);
+    }
 
-	/**
-	 * @deprecated Use {@link #setValues(Collection)}, which allows for values other than String
-	 */
-	@Deprecated
-	public void setAllowedValues(final String[] allowedValues) {
-		getExtendedProperties().put(PROPERTY_ALLOWED_VALUE, allowedValues);
-	}
+    /**
+     * @deprecated Use {@link #getValues()}, which allows for values other than String
+     */
+    @Deprecated
+    public String[] getAllowedValues() {
+        // Be compatible with the old behavior and only include String values.
+        ArrayList<String> stringValues = new ArrayList<String>();
+        Collection<?> values = (Collection<?>) getExtendedProperties().get(PROPERTY_ALLOWED_VALUE);
+        for (Object o : values) {
+            if (o instanceof String) {
+                stringValues.add((String) o);
+            }
+        }
+
+        return stringValues.toArray(new String[stringValues.size()]);
+    }
+
+    /**
+     * @deprecated Use {@link #setValues(Collection)}, which allows for values other than String
+     */
+    @Deprecated
+    public void setAllowedValues(final String[] allowedValues) {
+        getExtendedProperties().put(PROPERTY_ALLOWED_VALUE, allowedValues);
+    }
 }
