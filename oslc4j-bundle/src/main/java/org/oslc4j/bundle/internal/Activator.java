9b86f3c776724a382ffe75250507c1b5d49dfea0
Yash Khatri <yash.s.khatri@gmail.com>
2018-07-11 15:53:57 +0200

    Add the OSGi bundle for Lyo Core (#16)
    - Added Lyo-Validation and Store Core packages.
    - Changes jena-libs dependency and removed lyo.validation from
    oslc4j-bundle.
    
    Signed-off-by: Yash Khatri <yash.khatri@scania.com>
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

---
+++ oslc4j-bundle/src/main/java/org/oslc4j/bundle/internal/Activator.java
@@ -0,0 +1,17 @@
+package org.oslc4j.bundle.internal;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
