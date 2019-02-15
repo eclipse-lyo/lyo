425814c6d2367e4596c9f6b63d6ed4da5151e779
Andrew Berezovskyi <andriib@kth.se>
2019-02-15 22:51:20 +0100

    Add a sync script
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

---
+++ misc/sync-labels.rb
@@ -0,0 +1,33 @@
+projects = [
+  'eclipse/lyo',
+
+  'eclipse/lyo.core',
+  'eclipse/lyo.client',
+  'eclipse/lyo.server',
+  'eclipse/lyo.designer',
+
+  'eclipse/lyo.store',
+  'eclipse/lyo.trs-client',
+  'eclipse/lyo.trs-server',
+  'eclipse/lyo.validation',
+
+  'eclipse/lyo.docs',
+  'eclipse/lyo.rio',
+
+  'eclipse/lyo.domains',
+  'eclipse/lyo.ldp',
+  'eclipse/lyo.testsuite',
+]
+
+if not ENV.has_key?('GITHUB_ACCESS_TOKEN')
+  puts 'You shall set \'GITHUB_ACCESS_TOKEN\' env var before running this script'
+  exit
+end
+
+projects.each { |p|
+  # '-A' keeps unknown labels
+  # '-d' makes a dry run
+  res = %x( github-label-sync -l labels.json -A #{p} )
+  puts res
+  puts "="*79
+}
