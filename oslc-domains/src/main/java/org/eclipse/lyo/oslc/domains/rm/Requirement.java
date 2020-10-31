84a9616b1e8dc009c36d55640f69a95556ee3db1
Andrew Berezovskyi <andriib@kth.se>
2020-10-31 12:42:01 +0100

    Override Requirement toString
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- oslc-domains/src/main/java/org/eclipse/lyo/oslc/domains/rm/Requirement.java
+++ oslc-domains/src/main/java/org/eclipse/lyo/oslc/domains/rm/Requirement.java
@@ -221,6 +221,8 @@ public class Requirement
         }
     
         // Start of user code toString_finalize
+        result = String.format("%s: %s (Requirement; id=%s)", this.getShortTitle(), this.getTitle(), this.getIdentifier());
+
         // End of user code
     
         return result;
