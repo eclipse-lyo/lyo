79e1dc6f8bec6ac625a9ef5c69b88297047aa296
Jad El-khoury <jad@kth.se>
2022-03-01 00:21:29 +0100

    failing test for mostConcreteResourceType

---
+++ core/oslc4j-jena-provider/src/test/java/org/eclipse/lyo/oslc4j/provider/jena/resources/WildDog.java
@@ -0,0 +1,19 @@
+package org.eclipse.lyo.oslc4j.provider.jena.resources;
+
+import org.eclipse.lyo.oslc4j.core.annotation.OslcNamespace;
+import org.eclipse.lyo.oslc4j.core.annotation.OslcResourceShape;
+
+/**
+ * A dog pet
+ * @author rherrera
+ */
+@OslcNamespace("http://locahost:7001/vocabulary/")
+@OslcResourceShape(title = "AbstractTypesTest")
+public class WildDog extends Dog {
+
+    @Override
+    public void eat() {
+        System.out.println("Eating like a wild dog");
+    }
+    
+}
