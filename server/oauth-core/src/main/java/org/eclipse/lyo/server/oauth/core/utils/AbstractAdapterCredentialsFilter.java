c572978e844aca543edf571113342486671e3c53
Jad El-khoury <jad.elkhoury@lynxwork.com>
2023-06-14 16:14:22 +0200

    changes in how to log

--- server/oauth-core/src/main/java/org/eclipse/lyo/server/oauth/core/utils/AbstractAdapterCredentialsFilter.java
+++ server/oauth-core/src/main/java/org/eclipse/lyo/server/oauth/core/utils/AbstractAdapterCredentialsFilter.java
@@ -362,7 +362,7 @@ abstract public class AbstractAdapterCredentialsFilter<Credentials, Connection> 
 					{
 						sendUnauthorizedResponse(response, e);
 						//TODO: Change to a log message.
-			            log.info("UnauthorizedException occured while checking for Basic authentication: {}", e.getMessage());
+			            log.debug("UnauthorizedException occured while checking for Basic authentication: {}");
 						//Do not call chain.doFilter().
 						return;
 					} catch (ServletException ce)
