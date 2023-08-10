d0a6bc1e0c885219b19d2857b9fb83e9c4a27e6e
Andrew Berezovskyi <andriib@kth.se>
2023-08-11 01:11:41 +0200

    feat: script to update README table automatically
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

---
+++ tools/readme-matrix/update-readme.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -uo pipefail
+set -e
+set -x
+
+SCRIPT_PATH=$(realpath "$0")
+SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
+
+trap "rm ${SCRIPT_DIR}/tmp.md" err exit SIGINT SIGTERM
+
+if ! [ -x "$(command -v pip-run)" ]; then
+   python3 -m pip install pip-run
+fi
+
+if ! [ -x "$(command -v sponge)" ]; then
+   brew install sponge
+fi
+
+
+pushd "${SCRIPT_DIR}"
+
+./readme-matrix-generator.py downstream-projects.yml > tmp.md
+
+sed -e '/<!-- END YAML TABLE -->/e cat tmp.md' -e '/<!-- BEGIN YAML TABLE -->/,//{//!d}' ../../README.md | sponge ../../README.md
+
+popd
