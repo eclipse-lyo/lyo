fd16252e98d8a87464edf6852655a86bd826b310
Andrew Berezovskyi <andriib@kth.se>
2024-10-05 18:34:55 +0200

    docs: update README project matrix
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- tools/readme-matrix/update-readme.sh
+++ tools/readme-matrix/update-readme.sh
@@ -9,13 +9,13 @@ SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
 
 trap "rm ${SCRIPT_DIR}/tmp.md" err exit SIGINT SIGTERM
 
-if ! [ -x "$(command -v pip-run)" ]; then
-   if [ -x "$(command -v pipx)" ]; then
-      pipx install pip-run
-   else
-      python3 -m pip install pip-run
-   fi
-fi
+# if ! [ -x "$(command -v pip-run)" ]; then
+#    if [ -x "$(command -v pipx)" ]; then
+#       pipx install pip-run
+#    else
+#       python3 -m pip install pip-run
+#    fi
+# fi
 
 if ! [ -x "$(command -v sponge)" ]; then
    brew install sponge || sudo apt install moreutils || sudo zypper in moreutils
@@ -24,7 +24,7 @@ fi
 
 pushd "${SCRIPT_DIR}"
 
-./readme-matrix-generator.py downstream-projects.yml > tmp.md
+pipx run ./readme-matrix-generator.py downstream-projects.yml > tmp.md
 
 # https://unix.stackexchange.com/a/737087/6317
 sed -e '/<!-- END YAML TABLE -->/e cat tmp.md' -e '/<!-- BEGIN YAML TABLE -->/,//{//!d}' ../../README.md | sponge ../../README.md
