d00ed9039fdde95e39d514504038d4bd45894055
Andrew Berezovskyi <andriib@kth.se>
2026-08-24 12:17:33 +0200

    Rework OAuth filter & cover with tests (#998)

--- server/oauth-core/src/test/java/org/eclipse/lyo/server/oauth/provider/core/AppTest.java
+++ server/oauth-core/src/test/java/org/eclipse/lyo/server/oauth/provider/core/AppTest.java
@@ -13,39 +13,16 @@
  */
 package org.eclipse.lyo.server.oauth.provider.core;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+class AppTest {
+    @Test
+    void app() {
+        assertTrue(true);
     }
 }
